82521cb2eb9d79ae9c025b32b2e3def190eddb82
Axel Brandenburg <brandenb@nordita.org>
2025-02-02 18:50:56 +0100

    current version; message generated automatically on: Linux norlx65.nordita.org (brandenb)

--- Makefile
+++ Makefile
@@ -2,7 +2,13 @@ default:
 	pdflatex cv
 	pdflatex pub
 	latex2html pub
+	./bin/noDL-in-html node1.html
+	./bin/noDL-in-html node2.html
+	./bin/noDL-in-html node3.html
+	./bin/noDL-in-html node4.html
+	./bin/noDL-in-html node5.html
 
 ci:
-	git add index.html images pub Own_Papers Makefile ORCID.html tex cv.html highlights README.md cv.pdf
+	git add index.html images pub Own_Papers Makefile ORCID.html tex cv.html highlights README.md cv.pdf bin
 	gitci
+
--- pub/images.pl
+++ pub/images.pl
@@ -2,11 +2,11 @@
 # Associate images original text with physical files.
 
 
-$key = q/Lambda;MSF=1.6;AAT/;
+$key = q/pi;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="15" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="$\Lambda$">|; 
+ WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\pi$">|; 
 
 $key = q/f;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -14,23 +14,23 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img4.png"
  ALT="$f$">|; 
 
-$key = q/p;MSF=1.6;AAT/;
+$key = q/Lambda;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="12" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$p$">|; 
-
-$key = q/pi;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$\pi$">|; 
+ WIDTH="15" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$\Lambda$">|; 
 
 $key = q/alpha;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="14" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="$\alpha$">|; 
+
+$key = q/p;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$p$">|; 
 
 1;
 
