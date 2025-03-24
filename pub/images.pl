b796cffe4e6e19b25ecb7e7de910f33b22050399
Axel Brandenburg <brandenb@nordita.org>
2025-03-24 07:02:02 +0100

    current version; message generated automatically on: Linux norlx65.nordita.org (brandenb)

--- pub/images.pl
+++ pub/images.pl
@@ -2,23 +2,17 @@
 # Associate images original text with physical files.
 
 
-$key = q/alpha;MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="14" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="$\alpha$">|; 
-
 $key = q/pi;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img2.png"
  ALT="$\pi$">|; 
 
-$key = q/f;MSF=1.6;AAT/;
+$key = q/Lambda;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="$f$">|; 
+ WIDTH="15" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$\Lambda$">|; 
 
 $key = q/p;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -26,11 +20,17 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img3.png"
  ALT="$p$">|; 
 
-$key = q/Lambda;MSF=1.6;AAT/;
+$key = q/f;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="15" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="$\Lambda$">|; 
+ WIDTH="13" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$f$">|; 
+
+$key = q/alpha;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\alpha$">|; 
 
 1;
 
