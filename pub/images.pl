13938b304a14d11b0f10d5a2173b11d277a30823
Axel Brandenburg <brandenb@nordita.org>
2025-07-18 09:28:25 +0200

    current version; message generated automatically on: Linux nl6 (brandenb)

--- pub/images.pl
+++ pub/images.pl
@@ -4,32 +4,27 @@
 
 $key = q/Lambda;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="15" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img5.png"
+ STYLE="height: 2.05ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img5.svg"
  ALT="$\Lambda$">|; 
 
 $key = q/alpha;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="14" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img1.png"
+ STYLE="height: 1.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
  ALT="$\alpha$">|; 
 
 $key = q/f;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
+ STYLE="height: 2.52ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img4.svg"
  ALT="$f$">|; 
 
 $key = q/p;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="12" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
+ STYLE="height: 1.91ex; vertical-align: -0.57ex; " SRC="|."$dir".q|img3.svg"
  ALT="$p$">|; 
 
 $key = q/pi;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
+ STYLE="height: 1.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img2.svg"
  ALT="$\pi$">|; 
 
 1;
